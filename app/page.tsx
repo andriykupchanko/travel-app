357db1a5a6bca188855e387ef368ff7d413af303
Andriy Kupchanko <80577186+andriykupchanko@users.noreply.github.com>
2023-10-31 00:43:55 +0200

    create nav bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
 
+import Navbar from '../components/Navbar';
 export default function Home() {
   return (
-      <h1>Hello</h1>
+      <h1 className="font-bold text-teal-500">
+        Hello
+      </h1>
+      
   )
 }
